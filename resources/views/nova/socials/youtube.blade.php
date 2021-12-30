cb4dd08166b24ac4ccab7b8beda2bee9d0e3220c
Norman Huth <norman@huth.it>
2021-12-31 00:50:43 +0100

    Justify YouTube preview on NOva Dashboard

--- resources/views/nova/socials/youtube.blade.php
+++ resources/views/nova/socials/youtube.blade.php
@@ -2,12 +2,14 @@
     <h3 class="flex mb-3 text-base text-80 font-bold">
         {{ __('Current YouTube Video') }}:
     </h3>
-    <div style=" position: relative; padding-bottom: 56.25%;">
-        <iframe src="https://www.youtube.com/embed/{{ $youtube->provider_id }}?rel=0&"
-                title=" {{ __('Current YouTube Video') }}"
-                allowfullscreen id="player"
-                style="border: 1; top: 0; left: 0; width: 100%; height: 100%; position: absolute;"
-        ></iframe>
+    <div style="max-width: 33rem;" class="mx-auto">
+        <div style="position: relative; padding-bottom: 56.25%;">
+            <iframe src="https://www.youtube.com/embed/{{ $youtube->provider_id }}?rel=0&"
+                    title=" {{ __('Current YouTube Video') }}"
+                    allowfullscreen id="player"
+                    style="border: none; top: 0; left: 0; width: 100%; height: 100%; position: absolute; border-radius: .25rem"
+            ></iframe>
+        </div>
     </div>
     <div class="text-center mt-2">
         <a href="https://www.youtube.com/watch?v={{ $youtube->provider_id }}" class="no-underline dim text-primary font-bold" target="_blank" rel="noopener">https://www.youtube.com/watch?v={{ $youtube->provider_id }}</a>
